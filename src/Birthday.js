d1c18cc1ff6aa6c751d0be04f9972d1b22202789
Toni Mahilainen <toni.mahilainen@profitsoftware.com>
2021-09-16 19:27:02 +0300

    Complete birthday card

---
+++ src/Birthday.js
@@ -0,0 +1,134 @@
+import React from "react";
+import Confetti from "react-dom-confetti";
+import { css, injectGlobal } from "@emotion/css";
+import presentCard from "../src/img/present.PNG"
+
+class Birthday extends React.Component {
+  state = {
+    item: "🎁",
+    showPicture: false,
+    confetti: false,
+    hint: "(don't forget to open your present!)"
+  };
+
+  openPresent = () => {
+    this.setState({
+      showPicture: true,
+      confetti: true,
+      hint: ""
+    });
+  };
+
+  render() {
+    const style = {
+      fontFamily: "Fredoka One"
+    };
+    const config = {
+      angle: 90,
+      spread: 45,
+      startVelocity: 45,
+      elementCount: 50,
+      decay: 0.9
+    };
+
+    return (
+      <div>
+        <div className={titleContainer}>
+          <div style={style} className={title}>
+            Happy Birthday!
+          </div>
+          <div style={style} className={hint}>
+            {this.state.hint}
+          </div>
+        </div>
+        <div className={presentContainer}>
+          <Confetti active={this.state.confetti} config={config} />
+          <button className={presentButton} onClick={this.openPresent}>
+            {this.state.showPicture ? <img className={presentCardStyle} src={presentCard} /> : this.state.item}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Birthday;
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Fredoka+One');
+  body {}
+  body { color: black; }
+`;
+const titleContainer = css`
+  display: flex;
+  justify-content: center;
+  margin-top: 300px;
+  text-align: center;
+  flex-direction: column;
+  @media only screen and (max-width: 600px) {
+    margin-top: 250px;
+  }
+`;
+const presentContainer = css`
+  display: flex;
+  justify-content: center;
+`;
+const presentButton = css`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 50px;
+
+  &:focus {
+    outline: 0;
+  }
+
+  @media only screen and (max-width: 600px) {
+    @keyframes opacity-change {
+      0% {
+        transform: rotate(0deg);
+      }
+      25% {
+        transform: rotate(20deg);
+      }
+      50% {
+        transform: rotate(-20deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+
+    animation: opacity-change 1s infinite;
+  }
+  &:hover {
+    @keyframes opacity-change {
+      0% {
+        transform: rotate(0deg);
+      }
+      25% {
+        transform: rotate(20deg);
+      }
+      50% {
+        transform: rotate(-20deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+
+    animation: opacity-change 1s infinite;
+  }
+`;
+const title = css`
+  font-size: 50px;
+`;
+const hint = css`
+  font-size: 14px;
+  margin: 10px 0;
+`;
+
+const presentCardStyle = css`
+  width: 60%;
+  margin-top: 10px;
+`;
